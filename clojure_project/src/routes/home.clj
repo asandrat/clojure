8949d8eedc241cf6744ea358243f387257cf6732
aleksandra.tutoric <Sandra@Sandra-PC>
2017-03-26 22:31:17 +0200

    stylesheet

--- clojure_project/src/routes/home.clj
+++ clojure_project/src/routes/home.clj
@@ -10,12 +10,11 @@
 (defn indexpage []
   (layout/common
     [:div {:id "content"}
-    [:h1 {:class "text-success"} "Welcome to my fruitstore"]
+    [:h1 {:style "color: orange;"} "Hi! Welcome to my fruitstore"]
     [:br]
     [:br]
-    [:a {:href "/show"} "Show available fruits" ]
-    [:br]
-    [:a {:href "/requested"} "Show requested fruits" ]]))
+    [:h2 [:a {:href "/show" :style "text-decoration: none;"} "Show available fruits &nbsp;&nbsp;" ]]
+    [:h2 [:a {:href "/requested" :style "text-decoration: none;"} "&nbsp;&nbsp;Show requested fruits" ]]]))
 
 
 (defn show-fruits []
@@ -39,37 +38,40 @@
             [:td (:unit fruit)]
             [:td (:descent fruit)]
             [:td (:currency fruit)]
-            [:td [:a {:href (str "/buy/" (h (:id fruit)))} "buy"]]]))])
+            [:td [:a {:href (str "/buy/" (h (:id fruit))) :style "text-decoration: none;"} "Buy"]]]))])
 
 (defn show-requested-fruits []
-  [:table {:border 1}
+  (if (> (count (crud/read-request-fruit)) 0)
+  [:table {:style "color:grey;"}
    [:thead
     [:tr
-     [:th "Name"]
-     [:th "Quantity"]
-     [:th "Descent"]
-     [:th "Update"]]]
+     [:th {:style "color:orange;"} "Name&nbsp;" ]
+     [:th {:style "color:orange;"} "Quantity&nbsp;"]
+     [:th {:style "color:orange;"} "Descent&nbsp;"]
+     [:th ""]
+     [:th ""]]]
    (into [:tbody]
          (for [fruit (crud/read-request-fruit)]
            [:tr
             [:td (:name fruit)]
             [:td (:quantity fruit)]
             [:td (:descent fruit)]
-            [:td [:a {:href (str "/delete/" (h (:id fruit)))} "delete"]]
-            [:td [:a {:href (str "/update/" (h (:id fruit)))} "update"]]]))])
+            [:td [:a {:href (str "/delete/" (h (:id fruit))) :style "text-decoration: none;"} "Delete&nbsp;"]]
+            [:td [:a {:href (str "/update/" (h (:id fruit))) :style "text-decoration: none;"} "Update"]]]))]))
 
 (defn update-qty [& [name price currency quantity reqqty unit descent id error]]
   (layout/common
-  [:h2 "Buy fruit"]
-   [:a {:href "/" :class "back"} "Go Back"]
+  [:h1 {:style "color:blue;"} "Buy fruit"]
   (form-to {:id "frmBuy"}
     [:post "/buy"]
-           [:p "Id:"]
+           [:p {:style "color:red;"} error]
+           [:hr]
+            [:p "Id:"]
            (text-field {:readonly true} "id" id)
            [:p "Name:"]
            (text-field {:readonly true} "name" name)
            [:p "Price:" ]
-           (text-field {:id "price"} "price" price)
+           (text-field {:readonly true} "price" price)
            [:p "Currency:"]
            (text-field {:readonly true} "currency" currency)
            [:p "unit:"]
@@ -79,20 +81,21 @@
            [:p "Requsted quantity:"]
            (text-field "reqqty" reqqty)
            [:br] [:br]
-           (submit-button {:onclick " return javascript:validateInsertForm()"} "Buy")
+           (submit-button "Buy")
            [:hr]
-           [:p {:style "color:red;"} error])))
+           [:a {:href "/" :class "back"} "Go Back"])))
 
-(defn insert_update [& [name price currency quantity unit descent error id]]
+(defn insert-update [& [name price currency quantity unit descent id error]]
   (layout/common
-  [:h2 (if (nil? id) "Request fruit" "Buy fruit")]
-   [:a {:href "/" :class "back"} "Go Back"]
+  [:h2 (if (nil? id) "Request fruit" "Update requsted fruit")]
   (form-to {:id "frm_insert"}
     [:post "/save"]
+           [:p {:style "color:red;"} error]
+           [:hr]
            (if (not (nil? id))
-             [:p "Id:"])
+           [:p "Id:"])
            (if (not (nil? id))
-               (text-field {:readonly true} "id" id))
+           (text-field {:readonly true} "id" id))
            [:p "Name:"]
            (text-field "name" name)
            [:p "Price:" ]
@@ -106,9 +109,10 @@
            [:p "Descent:"]
            (text-field "descent" descent)
            [:br] [:br]
-           (submit-button {:onclick " return javascript:validateInsertForm()"} (if (nil? id)"Insert" "Update"))
+           (submit-button "Request")
            [:hr]
-           [:p {:style "color:red;"} error])))
+           [:br]
+           [:a {:href "/" :class "back"} "Go Back"])))
 
 (defn parse-number [s]
   (if (re-find #"^-?\d+\.?\d*$" s)
@@ -117,17 +121,17 @@
 (defn save-fruit [name price currency quantity unit descent & [id]]
   (cond
     (empty? name)
-    (insert_update  name price currency quantity unit descent "Fruit name must be entered" id)
+    (insert  name price currency quantity unit descent "Fruit name must be entered" id)
     (nil? (parse-number price))
-    (insert_update  name price currency quantity unit descent "Price must be a number" id)
+    (insert name price currency quantity unit descent "Price must be a number" id)
     (empty? currency)
-    (insert_update  name price currency quantity unit descent "Currency must be entered!" id)
+    (insert name price currency quantity unit descent "Currency must be entered!" id)
     (<= (parse-number quantity) 0)
-    (insert_update  name price currency quantity unit descent "Enter quantity!" id)
+    (insert name price currency quantity unit descent "Enter quantity!" id)
     (empty? unit)
-    (insert_update  name price currency quantity unit descent "Please fill unit" id)
+    (insert name price currency quantity unit descent "Please fill unit" id)
     (empty? descent)
-    (insert_update  name price currency quantity unit descent "Enter descent" id)
+    (insert name price currency quantity unit descent "Enter descent" id)
     :else
   (do (crud/save-fruit name price currency quantity unit descent 0)
   (ring/redirect "/requested"))))
@@ -153,7 +157,7 @@
   (ring/redirect "/requested"))
 
 (defn show-fruit [fruit]
-  (insert_update (:name fruit) (:price fruit) (:currency fruit) (:quantity fruit) (:unit fruit) (:descent fruit) nil (:id fruit)))
+  (insert (:name fruit) (:price fruit) (:currency fruit) (:quantity fruit) (:unit fruit) (:descent fruit) (:id fruit) nil))
 
 (defn update-fruit-qty [fruit]
   (update-qty (:name fruit) (:price fruit) (:currency fruit) (:quantity fruit) (:reqqty fruit) (:unit fruit) (:descent fruit) (:id fruit) nil))
@@ -162,12 +166,12 @@
   (layout/common
     [:h1 "fruits"]
     (show-fruits)
-    [:a {:href "/" :class "back"} "Home"]))
+    [:a {:href "/" :class "back" } "Home"]))
 
 (defn requested []
   (layout/common
-    [:h1 "fruits"]
-    [:a {:href "/add"} "Request new type of fruit" ]
+    [:h1 "Requested Fruits" ]
+    [:a {:href "/add" :style "text-decoration: none;"} "Request new type of fruit" ]
     [:br]
     (show-requested-fruits)
     [:a {:href "/" :class "back"} "Home"]))
@@ -176,8 +180,8 @@
   (GET "/" [] (indexpage))
   (GET "/show" [] (show))
   (GET "/requested" [] (requested))
-  (GET "/add" [] (insert_update))
-  (GET "/add" [name price currency quantity unit descent error id] (insert_update name price currency quantity unit descent error id))
+  (GET "/add" [] (insert))
+  (GET "/add" [name price currency quantity unit descent id error] (insert name price currency quantity unit descent id error))
   (POST "/save" [name price currency quantity unit descent id] (save-fruit name price currency quantity unit descent id))
   (POST "/buy" [name price currency quantity reqqty unit descent id] (buy-fruit name price currency quantity reqqty unit descent id))
   (GET "/buy/:id" [id] (update-fruit-qty (crud/find-fruit id)))
